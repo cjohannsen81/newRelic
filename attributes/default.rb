5edb042f5294a67da07a5dbe150df87ae5e27803
root <root@chef.cloud.lab>
2014-04-08 05:22:32 -0700

    new attributes set

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,16 +3,16 @@ when 'windows'
   if node['kernel']['machine'] == 'x86_64'
     default['newRelic']['agent']['uri'] = 'http://download.newrelic.com/windows_server_monitor/release/NewRelicServerMonitor_x64_3.1.17.0.msi'
     default['newRelic']['agent']['tempUri'] = "C:/chef/NewRelicServerMonitor_x64_3.1.17.0.msi"
-    default['newRelic']['agent']['license'] = "fca354a023a28874174ae9490c44d169e33772f6"
+    default['newRelic']['agent']['license'] = "YourLicense"
   else
     default['newRelic']['agent']['uri'] = 'http://download.newrelic.com/windows_server_monitor/release/NewRelicServerMonitor_x86_3.1.17.0.msi'
     default['newRelic']['agent']['tempUri'] = "C:/chef/NewRelicServerMonitor_x64_3.1.17.0.msi"
-    default['newRelic']['agent']['license'] = "fca354a023a28874174ae9490c44d169e33772f6"
+    default['newRelic']['agent']['license'] = "YourLicense"
   end
 when 'rhel'
   if node['kernel']['machine'] == 'x86_64'
     default['newRelic']['agent']['uri'] = 'http://download.newrelic.com/pub/newrelic/el5/i386/newrelic-repo-5-3.noarch.rpm'
     default['newRelic']['agent']['tempUri'] = "/tmp/newrelic-repo-5-3.noarch.rpm" 
-    default['newRelic']['agent']['license'] = "fca354a023a28874174ae9490c44d169e33772f6"
+    default['newRelic']['agent']['license'] = "YourLicense"
   end
 end
